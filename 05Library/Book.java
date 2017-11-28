849f162bc176741652a81b162b90248d209cd698
jmcgrathstuy <jmcgrath@stuy.edu>
2017-11-28 15:15:24 -0500

    Added variables and both constructors to Book.java

--- 05Library/Book.java
+++ 05Library/Book.java
@@ -1,7 +1,18 @@
 public class Book{
 
+    private String author;
+    private String title;
+    private String ISBN;
 
+    public Book(){}
 
+    public Book(String authorIn, String titleIn, String ISBNIn){
+	author = authorIn;
+	title = titleIn;
+	ISBN = ISBNIn;
+    }
+
+    
 
 
 
