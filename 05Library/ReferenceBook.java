5c02119d1ebecaa07a242de0c36b48e0d4559c71
jmcgrathstuy <jmcgrath@stuy.edu>
2017-11-29 22:41:00 -0500

    Added methods to ReferenceBook. Will test next

--- 05Library/ReferenceBook.java
+++ 05Library/ReferenceBook.java
@@ -14,6 +14,22 @@ public class ReferenceBook extends LibraryBook{
     public void setCollection(String collectionIn){
 	    collection = collectionIn;
     }
+	
+	public void checkout(String patron, String due){
+		System.out.println("cannot check out a reference book");
+	}
+	
+	public void returned(){
+		System.out.println("reference  book could not have been checked out -- return impossible");
+	}
+	
+	public String circulationStatus(){
+		return "non-circulating reference book";
+	}
+	
+	public String toString(){
+		return super.toString() + ", " + this.getCollection;
+	}
 
 
 }
